23e643b7d4d3b490821fc7188be99b0babd02e71
mdanassaifi <cscanas533@gmail.com>
2025-08-01 13:56:17 +0530

    js

---
+++ 01_basic/variable.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "cscan.1999@gmail.com"
+var accountPassword = "123456"
+accountCity = "Beijing"
+let accountState;
+
+accountEmail = "abcd@gmail.com"
+accountPassword = "000000"
+accountCity = "Shanghai"
+
+
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
