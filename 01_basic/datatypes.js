226d1c33801aed0330d5d52faeebde6700c6b7a8
mdanassaifi <cscanas533@gmail.com>
2025-08-02 12:07:59 +0530

    js

--- 01_basic/datatypes.js
+++ 01_basic/datatypes.js
@@ -1,7 +1,17 @@
-"use strict"; //treat all js codeas newer version 
+// "use strict"; //treat all js codeas newer version 
 
-// alert(3+3); we are using Node.js not browswr
+// // alert(3+3); we are using Node.js not browswr
 
-console.log(3+3)
+// console.log(3+3)
 
-console.log("anas")
+// console.log("anas")
+
+
+let age = "33"
+
+console.log(typeof age);
+console.log(typeof(age));
+
+let valueinNumber = Number(age);
+
+console.log(typeof valueinNumber);
