dcb71dcd855774072f188d75afd54c5908796666
mdanassaifi <cscanas533@gmail.com>
2025-08-05 12:22:58 +0530

    js

--- 01_basic/comparison.js
+++ 01_basic/comparison.js
@@ -5,3 +5,17 @@
 // console.log(2 <= 1);
 // console.log(2 == 1);
 // console.log(2 != 1);
+
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+
+
+// ===
+
+console.log(1 === 1);
