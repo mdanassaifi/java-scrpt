215fe09bca4828ac22cfea30d8b74409afb163cb
mdanassaifi <cscanas533@gmail.com>
2025-08-05 14:53:36 +0530

    js

---
+++ 01_basic/datatypes-summ.js
@@ -0,0 +1,38 @@
+// // primitive datatypes
+
+// 7 types : string , number , boolean ,
+// null , undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = true;
+const outsidetemp = null;
+
+let useEmail = undefined;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId); // false, because symbols are unique
+
+const bigNumber = 355555887888444488n
+
+const heros = ["shaktimsn", "superman", "batman"];
+let myhero = {
+    name: "batman",
+    age: 30,
+
+}
+
+const myFunction = function () {
+
+    console.log("hello world");
+};
+
+
+
+// refrence type ( non primitive datatypes ) 
+// object , array , function
+
+
