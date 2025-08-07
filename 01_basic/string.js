0cb80cfb5aa7ae9b26dbbbc9f03a7a9a9eea50e3
mdanassaifi <cscanas533@gmail.com>
2025-08-07 14:00:45 +0530

    js

---
+++ 01_basic/string.js
@@ -0,0 +1,8 @@
+const name = "ANAS"
+const repocount = 20
+
+// console.log(name +  repocount);
+
+console.log(`My name is ${name} and I have ${repocount} repositories.`);
+
+const gamename = new String("Super Mario");
