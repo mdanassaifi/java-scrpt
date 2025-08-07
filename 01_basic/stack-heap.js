b0b9a3c86b70dc650d6b720dd5028160c0629100
mdanassaifi <cscanas533@gmail.com>
2025-08-07 11:24:20 +0530

    js

--- 01_basic/stack-heap.js
+++ 01_basic/stack-heap.js
@@ -7,3 +7,16 @@ getuser = username
 console.log(getuser);
 
 
+getdata = {
+    name: 'saifi',
+    age: '24',
+    upi: 'nnnnnn@okhdfcbank.com'
+}
+
+
+let getdata2 = getdata
+
+getdata2.name = 'munna-bhi'
+
+console.log(getdata);
+console.log(getdata2);
