1f1b4f55f6cedb5f035b049686fc21952509cb93
mdanassaifi <cscanas533@gmail.com>
2025-08-10 13:21:00 +0530

    js

--- 01_basic/number_and_math.js
+++ 01_basic/number_and_math.js
@@ -1,33 +1,33 @@
-// const score = 400
-// console.log(score);
+const score = 400
+console.log(score);
 
-// const balance = new Number(4000);
-// console.log(balance);
+const balance = new Number(4000);
+console.log(balance);
 
-// console.log(balance.toString());
-// console.log(balance.toFixed(1));
+console.log(balance.toString());
+console.log(balance.toFixed(1));
 
-// const othernumber = 23.89999
+const othernumber = 23.89999
 
-// console.log(othernumber.toPrecision(4));
-// console.log(othernumber.toFixed(1));
+console.log(othernumber.toPrecision(4));
+console.log(othernumber.toFixed(1));
 
-// const hundred = 1000000;
+const hundred = 1000000;
 
-// console.log(hundred.toLocaleString('en-IN'));
+console.log(hundred.toLocaleString('en-IN'));
 
 // *************************MATH**********************
 
 
 
-// console.log(Math);
+console.log(Math);
 
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.8));
-// console.log(Math.min(4.2, 4.8));
-// console.log(Math.max(4.2, 4.8));
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.8));
+console.log(Math.min(4.2, 4.8));
+console.log(Math.max(4.2, 4.8));
 
 console.log(Math.random());
 console.log((Math.random()*5) + 1);
