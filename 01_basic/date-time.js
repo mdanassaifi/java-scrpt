e0b0c81d1968b5e53176b6b98735fd6b613c474a
mdanassaifi <cscanas533@gmail.com>
2025-08-10 15:48:06 +0530

    js

--- 01_basic/date-time.js
+++ 01_basic/date-time.js
@@ -30,3 +30,17 @@ console.log(newdate.getMonth());
 console.log(newdate.getDay());
 
 
+
+console.log(newDate.toLocaleString('default', 
+    {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        timeZoneName: 'short'
+    }
+));
+
