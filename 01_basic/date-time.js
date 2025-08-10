1cedcc176925ad6ef717863e78f23f3b55124059
mdanassaifi <cscanas533@gmail.com>
2025-08-10 14:55:38 +0530

    js

--- 01_basic/date-time.js
+++ 01_basic/date-time.js
@@ -4,4 +4,9 @@ console.log(myDate);
 console.log(myDate.toString());
 console.log(myDate.toDateString());
 console.log(myDate.toLocaleString());
+console.log(myDate.toJSON());
+console.log(myDate.toTimeString());
+
+console.log(typeof myDate);
+
 
