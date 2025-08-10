b0aef96d8b56dd58d0ab288fef3c66c26d86e31f
mdanassaifi <cscanas533@gmail.com>
2025-08-10 12:57:06 +0530

    js

--- 01_basic/number_and_math.js
+++ 01_basic/number_and_math.js
@@ -3,3 +3,16 @@ console.log(score);
 
 const balance = new Number(4000);
 console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toFixed(1));
+
+const othernumber = 23.89999
+
+console.log(othernumber.toPrecision(4));
+console.log(othernumber.toFixed(1));
+
+const hundred = 1000000;
+
+console.log(hundred.toLocaleString('en-IN'));
+
