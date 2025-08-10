15e7d50bad4d2e8fa646c86ba8dac3740084a468
mdanassaifi <cscanas533@gmail.com>
2025-08-10 13:06:31 +0530

    js

--- 01_basic/number_and_math.js
+++ 01_basic/number_and_math.js
@@ -20,11 +20,12 @@
 
 
 
-console.log(Math);
+// console.log(Math);
 
-console.log(Math.abs(-4));
-console.log(Math.round(4.6));
-console.log(Math.ceil(4.2));
-console.log(Math.floor(4.8));
-console.log(Math.min(4.2, 4.8));
-console.log(Math.max(4.2, 4.8));
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(4.2, 4.8));
+// console.log(Math.max(4.2, 4.8));
+
