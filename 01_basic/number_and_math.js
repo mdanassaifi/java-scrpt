95ac2ac4c4355e25455dfaaf9b95507683571a53
mdanassaifi <cscanas533@gmail.com>
2025-08-10 13:19:46 +0530

    js

--- 01_basic/number_and_math.js
+++ 01_basic/number_and_math.js
@@ -30,4 +30,9 @@
 // console.log(Math.max(4.2, 4.8));
 
 console.log(Math.random());
-console.log((Math.random()*10) + 1);
+console.log((Math.random()*5) + 1);
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.floor(Math.random() * (max - min + 1)) + min));
