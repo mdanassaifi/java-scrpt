4f95ad75f02639aadbb9198e826cb2b2b4e54f06
mdanassaifi <cscanas533@gmail.com>
2025-08-10 12:58:38 +0530

    js

--- 01_basic/number_and_math.js
+++ 01_basic/number_and_math.js
@@ -1,18 +1,23 @@
-const score = 400
-console.log(score);
+// const score = 400
+// console.log(score);
 
-const balance = new Number(4000);
-console.log(balance);
+// const balance = new Number(4000);
+// console.log(balance);
 
-console.log(balance.toString());
-console.log(balance.toFixed(1));
+// console.log(balance.toString());
+// console.log(balance.toFixed(1));
 
-const othernumber = 23.89999
+// const othernumber = 23.89999
 
-console.log(othernumber.toPrecision(4));
-console.log(othernumber.toFixed(1));
+// console.log(othernumber.toPrecision(4));
+// console.log(othernumber.toFixed(1));
 
-const hundred = 1000000;
+// const hundred = 1000000;
 
-console.log(hundred.toLocaleString('en-IN'));
+// console.log(hundred.toLocaleString('en-IN'));
+
+// *************************MATH**********************
+
+
+
 
