74ed50b4430d3a4068cd3f441a95d6721e993ae7
mdanassaifi <cscanas533@gmail.com>
2025-08-10 15:00:28 +0530

    js

--- 01_basic/date-time.js
+++ 01_basic/date-time.js
@@ -1,12 +1,14 @@
-let myDate = new Date();
+// let myDate = new Date();
 
-console.log(myDate);
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
-console.log(myDate.toJSON());
-console.log(myDate.toTimeString());
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toTimeString());
 
-console.log(typeof myDate);
+// console.log(typeof myDate);
 
+let myCreateDate = new Date('2025-08-10');
+console.log(myCreateDate.toDateString());
 
