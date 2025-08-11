b2ef8ced6ca3589e8d88066d9170569f18ca8fe2
mdanassaifi <cscanas533@gmail.com>
2025-08-11 12:03:43 +0530

    js

--- 02_basic/array.js
+++ 02_basic/array.js
@@ -6,16 +6,17 @@ const myArr = [0,1, 2, 3, 4, 5];
 const myhero = [ 'Iron Man', 'Captain America', 'Thor', 'Hulk', 'Black Widow' ]
 
 const myarr2 = new Array(1, 2, 3, 4, 5)
-console.log(myArr[1]);
+// console.log(myArr[1]);
 
 // Array Methods
 
 
 // myArr.push(6);
-// myArr.push(7);
+// myArr.push(7); 
 // myArr.pop();
 
 
 myArr.unshift(9);
+myArr.shift();
 console.log(myArr);
 
