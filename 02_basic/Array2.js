d48189642d9b580d27aedd1a285809fe9d925019
mdanassaifi <cscanas533@gmail.com>
2025-08-11 14:42:33 +0530

    js

--- 02_basic/Array2.js
+++ 02_basic/Array2.js
@@ -1,4 +1,10 @@
 const marvel_heros = ['Iron Man', 'Captain America', 'Thor', 'Hulk', 'Black Widow'];
 const dc = ['Superman', 'Batman', 'Wonder Woman', 'Flash', 'Green Lantern'];
 
-marvel_heros.push(dc)
+// marvel_heros.push(dc)
+
+// console.log(marvel_heros);
+
+
+const all_heros = marvel_heros.concat(dc);
+console.log(all_heros);
