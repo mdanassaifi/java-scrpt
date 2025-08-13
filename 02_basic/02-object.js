55ba67f55e289cf26c7c7ee6e6334a95a4221391
mdanassaifi <cscanas533@gmail.com>
2025-08-13 14:05:58 +0530

    object literal

--- 02_basic/02-object.js
+++ 02_basic/02-object.js
@@ -21,4 +21,11 @@ const regularuser ={
 // console.log(regularuser.fullname);
 // console.log(regularuser.fullname.userfullname);
 
+const obj1 = {1: "a" , 2: "b"}
+const obj2= {3: "c", 4: "d"}
 
+// const obj3 = { obj1, obj2 }
+
+console.log(obj3);
+
+// const mergedObj = {...obj1, ...obj2}
