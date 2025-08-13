4d4fdd66495117c9f47d7f0b9e4159b33b4caf42
mdanassaifi <cscanas533@gmail.com>
2025-08-13 14:49:40 +0530

    object literal

--- 02_basic/02-object.js
+++ 02_basic/02-object.js
@@ -47,3 +47,9 @@ const user = [
         email: "user3@gmail.com"
     },
 ]
+
+user[1].email
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
