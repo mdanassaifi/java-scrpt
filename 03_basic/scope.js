05eb815a4e0de5970fb53d814b7b0a0f75ef8ac7
mdanassaifi <cscanas533@gmail.com>
2025-08-17 12:49:52 +0530

    scope

--- 03_basic/scope.js
+++ 03_basic/scope.js
@@ -3,10 +3,12 @@
 if (true) {
     let a = 10
     const b = 20
-    var c = 30
+    // var c = 30
+    console.log("inner: ", a);
 }
 
+let a = 100
 
-// console.log(a);
+console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
