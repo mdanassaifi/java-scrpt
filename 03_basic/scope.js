4a9d84962d62f9235d8fa562d0ecd5e9a456ba74
mdanassaifi <cscanas533@gmail.com>
2025-08-17 12:43:34 +0530

    scope

--- 03_basic/scope.js
+++ 03_basic/scope.js
@@ -1,15 +1,12 @@
-let a = 10;
-const b = 20;
-var c = 30;
 
 
+if (true) {
+    let a = 10
+    const b = 20
+    var c = 30
+}
 
 
-
-
-
-
-
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
 console.log(c);
