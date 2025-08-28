985ffe3d71aa6e5da8c014c480344d2b228d5cf8
mdanassaifi <cscanas533@gmail.com>
2025-08-28 13:15:58 +0530

    controlflow

--- 04_controlflow/one.js
+++ 04_controlflow/one.js
@@ -9,9 +9,20 @@
 // }
 
 const isUserLoggedIn = true;
+const temperature = 25;
 
-if (2 === "2"){
-    console.log("executed");
+// if (isUserLoggedIn) {
+//     console.log("User is logged in");
+// }
+
+if (temperature === 20) {
+    console.log("It's a hot day");
+} else {
+    console.log("temp is very hot");
 }
+
+// if (2 === "2"){
+//     console.log("executed");
+// }
 
 // <,> <=, >=, ===, !== 
