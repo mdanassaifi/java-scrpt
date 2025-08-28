045c2d3e92e08ad8ec65ed308c807092f285c6e9
mdanassaifi <cscanas533@gmail.com>
2025-08-28 16:16:37 +0530

    controlflow

--- 04_controlflow/one.js
+++ 04_controlflow/one.js
@@ -28,16 +28,20 @@ const temperature = 25;
 // <,> <=, >=, ===, !== 
 
 
-const score = 200
+// const score = 200
 
-if (score > 100) {
-    const power = "fly"
-    console.log(`user power : ${power}`);
-}
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`user power : ${power}`);
+// }
 
 
 
 const balance = 1000
-if (balance > 500) {
-    console.log("user can buy a phone");
+if (balance < 500) {
+    console.log("less than 500");
+} else if (balance < 750) { 
+    console.log("less than 750");
+} else {
+    console.log("less than 1200");
 }
