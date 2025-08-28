5c26d0930e4b1a78302b816051a9fa08438c0067
mdanassaifi <cscanas533@gmail.com>
2025-08-28 16:45:14 +0530

    switch

--- 04_controlflow/switch.js
+++ 04_controlflow/switch.js
@@ -1,8 +1,24 @@
-switch (key) {
-    case value:
-        
+const month = 3;
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+
+    case 2:
+        console.log("February");
+        break;
+
+    case 3:
+        console.log("March");
+        break;
+
+    case 4:
+        console.log("April");
         break;
 
     default:
+        console.log("Invalid month");
         break;
 }
+
