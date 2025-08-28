bc81af03af20032dd9ede326aa5ea4bd65c94cbe
mdanassaifi <cscanas533@gmail.com>
2025-08-28 13:35:36 +0530

    controlflow

--- 04_controlflow/one.js
+++ 04_controlflow/one.js
@@ -34,3 +34,10 @@ if (score > 100) {
     const power = "fly"
     console.log(`user power : ${power}`);
 }
+
+
+
+const balance = 1000
+if (balance > 500) {
+    console.log("user can buy a phone");
+}
