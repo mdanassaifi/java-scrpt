79e908779b6ad211f0144fcde7efd1dd7ef8a185
mdanassaifi <cscanas533@gmail.com>
2025-08-28 16:50:21 +0530

    truth

---
+++ 04_controlflow/tryth.js
@@ -0,0 +1,7 @@
+const userEmail = "user@example.com";
+
+if (userEmail === "user@example.com") {
+    console.log("User email is verified");
+} else {
+    console.log("User email is not verified");
+}
