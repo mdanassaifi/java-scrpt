a5e5cbf725c6454c04e1306951762e93ed190620
mdanassaifi <cscanas533@gmail.com>
2025-08-29 14:40:53 +0530

    trythy

--- 04_controlflow/tryth.js
+++ 04_controlflow/tryth.js
@@ -1,10 +1,10 @@
 const userEmail = "user@example.com";
 
-if (userEmail === "user@example.com") {
-    console.log("User email is verified");
-} else {
-    console.log("User email is not verified");
-}
+// if (userEmail === "user@example.com") {
+//     console.log("User email is verified");
+// } else {
+//     console.log("User email is not verified");
+// }
 
 
 // falsy values in JS
@@ -12,3 +12,12 @@ if (userEmail === "user@example.com") {
 
 // truthy values in JS
 // "hello", 42, -42, 3.14, true, {}, []
+
+// Nullish coalescing operator (??)
+
+let val1;
+// val1 = 5 ?? 10;
+val1 = null ?? 10;
+
+
+console.log(val1);
