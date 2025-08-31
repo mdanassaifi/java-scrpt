d6748bbd0fabd93d7fce01beaab955ff13030fcd
mdanassaifi <cscanas533@gmail.com>
2025-08-31 11:58:59 +0530

    loop

--- iteration/foor.js
+++ iteration/foor.js
@@ -12,10 +12,19 @@ for (let i = 0; i < 10; i++) {
 
 
 for (let i = 0; i < 10; i++) {
-    console.log(`outer loop value: ${i}`);
+    // console.log(`outer loop value: ${i}`);
     for (let j = 0; j <= 10; j++) {
-        console.log(`inner loop value: ${j} and inner loop ${i}`);
-        console.log(i + '*' + j + ' = ' + i * j);
+        // console.log(`inner loop value: ${j} and inner loop ${i}`);
+        // console.log(i + '*' + j + ' = ' + i * j);
     
     }
+}
+
+let myArray = ["anas" , "saifi" , "don"]
+console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+    
 }
