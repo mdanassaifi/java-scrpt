c296a746726cf142192135ee711e05b43c190850
mdanassaifi <cscanas533@gmail.com>
2025-09-07 14:00:52 +0530

    loop

--- iteration/for-of-loop.js
+++ iteration/for-of-loop.js
@@ -8,5 +8,24 @@ for (const num of array) {
 
 const greeting = "Hello World"
 for (const greet of greeting) {
-    console.log(greet);
+    // console.log(greet);
+}
+
+
+//  maps 
+
+const map = new Map();
+map.set("key1", "value1");
+map.set("key2", "value2");
+map.set("key3", "value3");
+map.set("key4", "value4");
+map.set("key5", "value5");
+map.set("key6", "value6");
+map.set("key7", "value7");
+map.set("key8", "value8");
+map.set("key9", "value9");
+map.set("key10", "value10");
+
+for (const [key, value] of map) {
+    console.log(key, value);
 }
