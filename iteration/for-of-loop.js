9ce333cb66239c7f06c1222dc937792c844f598d
mdanassaifi <cscanas533@gmail.com>
2025-09-07 13:59:26 +0530

    loop

--- iteration/for-of-loop.js
+++ iteration/for-of-loop.js
@@ -3,5 +3,10 @@
 const array = [1, 2, 3, 4, 5]
 
 for (const num of array) {
-    console.log(num);
+    // console.log(num);
+}
+
+const greeting = "Hello World"
+for (const greet of greeting) {
+    console.log(greet);
 }
