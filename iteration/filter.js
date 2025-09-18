d1edb4f9544162c949d05faf9bcc4f4a57987414
mdanassaifi <cscanas533@gmail.com>
2025-09-18 14:37:34 +0530

    filter

--- iteration/filter.js
+++ iteration/filter.js
@@ -1,4 +1,4 @@
 const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const mynum= myArray.filter((num) => num > 4)
-console.log(mynum);
+// const mynum= myArray.filter((num) => num > 4)
+// console.log(mynum);
